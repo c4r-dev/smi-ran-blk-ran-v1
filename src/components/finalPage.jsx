4b2a1a46ce35b1781101375668f3c6a7b6e2593d
David Fischer <fiscd@seas.upenn.edu>
2024-09-04 11:53:26 -0400

    build

--- src/components/finalPage.jsx
+++ src/components/finalPage.jsx
@@ -3,15 +3,15 @@ export default function FinalPage() {
     return (
 
         <>
-            <div className='finalPage'>
-                <div className="textLeft">
-                    <h1>BLOCK RANDOMIZATION</h1>
-                </div>
+            <div className='finalPage'>    
                 <div className='finalPageCenter'>
                     <br></br><br></br>
                     <br></br><br></br>
                     <br></br><br></br>
                     <br></br><br></br>
+                    <br></br><br></br>
+                    <br></br><br></br>
+                    <h1>BLOCK RANDOMIZATION</h1>
                     <h3>YOU'VE FINISHED THIS ACTIVITY</h3>
                     <h1>CONGRATULATIONS</h1>
                     {/* <h3>** place holder for additional text **</h3> */}
